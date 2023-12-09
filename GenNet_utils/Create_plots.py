72d2b7280302a296e1401458f43c941a02f8acdc
aj3114 <145288778+aj3114@users.noreply.github.com>
2023-12-09 17:00:20 -0500

    Commented Create_plots.py

--- GenNet_utils/Create_plots.py
+++ GenNet_utils/Create_plots.py
@@ -6,44 +6,59 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import seaborn as sns
+
+# Adding the directory above the current script's directory to the system path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from GenNet_utils.Utility_functions import get_paths
 
+# Function to plot loss curves from training and validation sets
 def plot_loss_function(resultpath):
+        # Load training log file
         log_file = pd.read_csv(resultpath + "/train_log.csv")
+        # Plotting loss and validation loss
         plt.plot(log_file['loss'])
         plt.plot(log_file['val_loss'])
         plt.title('Loss curve')
         plt.ylabel('loss')
         plt.xlabel('epoch')
         plt.legend(['train', 'val'], loc='upper right')
+        # Saving the plot as an image file
         plt.savefig(resultpath + "train_val_loss.png")
+        # Displaying the plot
         plt.show()
-
+        
+# Function to create a sunburst plot based on a CSV file with importance values
 def sunburst_plot(resultpath, importance_csv, num_layers=3, plot_threshold=0.01, add_end_node=True):
+# Copying the CSV file to avoid modifying the original dataframe
     csv_file = importance_csv.copy()
 
     number_of_weights = csv_file.filter(like="weights").shape[1]
-
+        
+# Optionally add an end node to the dataset
     if add_end_node:
+        # Creating a new column for the end node
         csv_file["node_layer_" + str(number_of_weights)] = 0
         csv_file["layer" + str(number_of_weights) + '_name'] = "Prediction"
-
+# Preparing layer names for the sunburst plot
     plot_layer_names = []
     for i in range(number_of_weights, number_of_weights - num_layers, -1):
         plot_layer_names.append("layer" + str(i) + '_name')
 
     first_number_layer_to_plot = i
 
+# Calculating the percentage contribution of each layer
     csv_file["percentage_0"] = csv_file.filter(like="weights").prod(axis=1)
     csv_file["percentage_0"] = abs(csv_file["percentage_0"]) / abs(csv_file["percentage_0"]).sum() * 100
     print(csv_file["percentage_" + str(0)].sum())
 
+# Aggregating percentages for each subsequent layer
     for i in range(1, first_number_layer_to_plot + 1):
         csv_file["percentage_" + str(i)] = csv_file.groupby("layer" + str(i) + '_name')[
             "percentage_" + str(i - 1)].transform('sum')
 
     print(csv_file["percentage_" + str(first_number_layer_to_plot)].sum())
+
+# Dropping duplicates and filtering based on threshold
     csv_file = csv_file.drop_duplicates("layer" + str(first_number_layer_to_plot) + '_name')
     print(csv_file["percentage_" + str(first_number_layer_to_plot)].sum())
 
@@ -54,6 +69,7 @@ def sunburst_plot(resultpath, importance_csv, num_layers=3, plot_threshold=0.01,
     print(csv_file.shape)
     print(csv_file["percentage_" + str(first_number_layer_to_plot)].sum())
     print("start plotting")
+ # Creating the sunburst plot using Plotly
     fig = px.sunburst(csv_file,
                       path=plot_layer_names,
                       values="percentage_" + str(first_number_layer_to_plot),
@@ -62,24 +78,29 @@ def sunburst_plot(resultpath, importance_csv, num_layers=3, plot_threshold=0.01,
                       color_discrete_sequence=px.colors.qualitative.G10
                       )
 
+# Saving the plot as an image and HTML file
     fig.write_image(resultpath + "sunburst.png")
     fig.write_html(resultpath + "sunburst.html")
     print(resultpath)
     print("done")
 
-
+# Function to plot layer weights in a network
 def plot_layer_weight(resultpath, importance_csv, layer=0, num_annotated=10):
     csv_file = importance_csv.copy()
+# Check if the specified layer is within the range of layers in the CSV file
     if int(layer) < csv_file.filter(like="weights").shape[1] - 1:
         pass
     elif int(layer) == csv_file.filter(like="weights").shape[1] - 1:
+        # If the layer is the last one, add an end node for plotting
         csv_file["node_layer_" + str(csv_file.filter(like="weights").shape[1])] = 0
         csv_file["layer" + str(csv_file.filter(like="weights").shape[1]) + '_name'] = "end_node"
     else:
         print("error cant plot that many layers, there are not that many layers")
         sys.exit()
 
+# Prepare the data for plotting based on whether chromosome information is available
     if 'chr' in csv_file.columns:
+        # Select relevant columns for plotting with chromosome information
         columns = ["node_layer_" + str(layer), "node_layer_" + str(layer + 1), "weights_" + str(layer),
                    'layer' + str(layer) + '_name', 'layer' + str(layer + 1) + '_name', 'chr']
         csv_file = csv_file[columns]
@@ -87,6 +108,7 @@ def plot_layer_weight(resultpath, importance_csv, layer=0, num_annotated=10):
 
         csv_file = csv_file.sort_values(by=['chr', 'node_layer_' + str(layer)], ascending=True)
     else:
+        # Select relevant columns for plotting without chromosome information
         columns = ["node_layer_" + str(layer), "node_layer_" + str(layer + 1), "weights_" + str(layer),
                    'layer' + str(layer) + '_name', 'layer' + str(layer + 1) + '_name']
         csv_file = csv_file[columns]
@@ -94,6 +116,7 @@ def plot_layer_weight(resultpath, importance_csv, layer=0, num_annotated=10):
 
         csv_file = csv_file.sort_values(by=['node_layer_' + str(layer)], ascending=True)
 
+# Assign positions for plotting and normalize the weights
     csv_file["pos"] = np.arange(len(csv_file))
     weights = abs(csv_file["weights_" + str(layer)].values)
     weights = weights / max(weights)
@@ -166,6 +189,7 @@ def plot_layer_weight(resultpath, importance_csv, layer=0, num_annotated=10):
         plt.savefig(resultpath + "Weights_layer_" + str(layer) + ".png", bbox_inches='tight', pad_inches=0)
 
 
+# Function to create a Manhattan plot for relative importance of features
 def manhattan_relative_importance(resultpath, importance_csv, num_annotated=10):
     csv_file = importance_csv.copy()
     plt.figure(figsize=(20, 10))
@@ -188,6 +212,7 @@ def manhattan_relative_importance(resultpath, importance_csv, num_annotated=10):
         color_end = np.insert(color_end, 0, 0)
         print("no chr information continuing by coloring per group in node_layer_1")
 
+# Assign positions for plotting and normalize the weights
     weights = abs(csv_file["raw_importance"])
     weights = weights / max(weights)
     csv_file["plot_weights"] = weights
@@ -227,12 +252,16 @@ def manhattan_relative_importance(resultpath, importance_csv, num_annotated=10):
 
     plt.savefig(resultpath + "Manhattan_relative_importance_SNPs.png", bbox_inches='tight', pad_inches=0)
 
-
+# Main function to generate plots based on command line arguments
 def plot(args):
+    # Getting paths for results
     folder, resultpath = get_paths(args)
+    # Loading importance values from CSV file
     importance_csv = pd.read_csv(resultpath + "/connection_weights.csv", index_col=0)
     print(resultpath)
+    # Selecting layer for plotting
     layer = args.layer_n
+    # Choosing the type of plot to generate based on command line arguments
     if args.type == "layer_weight":
         plot_layer_weight(resultpath, importance_csv, layer=layer, num_annotated=10)
     elif args.type == "sunburst":
@@ -241,4 +270,4 @@ def plot(args):
         manhattan_relative_importance(resultpath=resultpath, importance_csv=importance_csv)
     else:
         print("invalid type:", args.type)
-        exit()
+        sys.exit()
